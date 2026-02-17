c9d9fca3f26a9d445e21b85edf6066e868f402a4
GKvillain <hathaipat012@gmail.com>
2026-02-17 23:17:17 +0700

    update

--- js/statistic.js
+++ js/statistic.js
@@ -5,4 +5,6 @@ const supabaseKey = "sb_publishable_g7hW-cLcITvnV_PqY9CeVQ_fNpLlvaN";
 
 const db = createClient(supabaseUrl, supabaseKey);
 
-asyn function get
+const showHours = document.querySelector(".showHours");
+const selectMonth = document.getElementById("month");
+const selectYear = document.getElementById("year");
